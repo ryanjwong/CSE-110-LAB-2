2eab7ce6519d37d04633f65c63968ee595109baa
ryanjwong <ryanjwong007@gmail.com>
2024-10-14 16:20:33 -0700

    Task 4

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -24,59 +24,109 @@ function App() {
   const toggleTheme = () => {
     setCurrentTheme(currentTheme === themes.light ? themes.dark : themes.light);
   };
- 
+
+
+  const [notes, setNotes] = useState(dummyNotesList);
+  const initialNote = {
+    id: -1,
+    title: "",
+    content: "",
+    label: Label.other,
+  };
+  const [createNote, setCreateNote] = useState(initialNote);
+
+  const createNoteHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("title: ", createNote.title);
+    console.log("content: ", createNote.content);
+    createNote.id = notes.length + 1;
+    setNotes([createNote, ...notes]);
+    setCreateNote(initialNote);
+  };
+  const [selectedNote, setSelectedNote] = useState<Note>(initialNote);
+
+function deleteNote(note: Note) {
+  setNotes(notes.filter(item => item.id !== note.id))
+}
+
 
   return (
     <ThemeContext.Provider value={currentTheme}>
 
-    <div className='app-container' style={{ 
+      <div className='app-container' style={{
         backgroundColor: currentTheme === themes.light ? '#ffffff' : '#333333',
       }}>
-      <form className="note-form">
-        <div><input placeholder="Note Title"></input></div>
+        <form className="note-form" onSubmit={createNoteHandler}>
+    	<div>
+      	<input
+        	placeholder="Note Title"
+        	onChange={(event) =>
+          	setCreateNote({ ...createNote, title: event.target.value })}
+        	required>
+      	</input>
+    	</div>
 
-        <div><textarea></textarea></div>
+    	<div>
+      	<textarea
+        	onChange={(event) =>
+          	setCreateNote({ ...createNote, content: event.target.value })}
+        	required>
+      	</textarea>
+    	</div>
 
-        <div><button type="submit">Create Note</button></div>
-      </form>
+  <div>
+     	<select
+       	onChange={(event) =>
+         	setCreateNote({ ...createNote, label: event.target.value as Label})}
+       	required>
+       	<option value={Label.personal}>Personal</option>
+       	<option value={Label.study}>Study</option>
+       	<option value={Label.work}>Work</option>
+       	<option value={Label.other}>Other</option>
+     	</select>
+   	</div>
+
+    	<div><button type="submit">Create Note</button></div>
+  	</form>
 
 
-      <div className="notes-grid">
-        {dummyNotesList.map((note) => (
-          <div
-            key={note.id}
-            className="note-item">
-            <div className="notes-header">
-            <button onClick={() => handleFavorite(note)}>
-              {favorites.some(fav => fav.id === note.id) ? (
-                <FaHeart style={{ color: 'red' }} />
-              ) : (
-                <CiHeart />
-              )}
-            </button>
 
-              <button>x</button>
+        <div className="notes-grid">
+          {notes.map((note) => (
+            <div
+              key={note.id}
+              className="note-item">
+              <div className="notes-header">
+                <button onClick={() => handleFavorite(note)}>
+                  {favorites.some(fav => fav.id === note.id) ? (
+                    <FaHeart style={{ color: 'red' }} />
+                  ) : (
+                    <CiHeart />
+                  )}
+                </button>
+
+                <button onClick={() => deleteNote(note)}>x</button>
+              </div>
+              <h2 contentEditable="true"> {note.title} </h2>
+              <p contentEditable="true"> {note.content} </p>
+              <p contentEditable="true"> {note.label} </p>
             </div>
-            <h2> {note.title} </h2>
-            <p> {note.content} </p>
-            <p> {note.label} </p>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div>
+          <button onClick={toggleTheme}> Toggle Theme </button>
+        </div>
+        <div>
+          <h2>List of favorites:</h2>
+          <ul>
+            {favorites.map(note => (
+              <li key={note.id}>{note.title}</li>
+            ))}
+          </ul>
+        </div>
+
+
       </div>
-      <div>
-     <button onClick  ={toggleTheme}> Toggle Theme </button>
-     </div>
-      <div>
-        <h2>List of favorites:</h2>
-      <ul>
-        {favorites.map(note => (
-          <li key={note.id}>{note.title}</li>
-        ))}
-      </ul>
-      </div>
-      
- 
-    </div>
     </ThemeContext.Provider>
 
 
